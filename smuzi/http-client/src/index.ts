2920b0212cb9d246fcbf33b3dd329d941bd9c10b
Ratushniak DA <ratushniak_da@groupbwt.com>
2025-12-02 00:13:47 +0200

    t

--- smuzi/http-client/src/index.ts
+++ smuzi/http-client/src/index.ts
@@ -4,13 +4,20 @@ export type BaseRequestConfig = {
     method: HttpMethod;
     headers: RequestHttpHeaders;
     query: Record<string, string | number | boolean>;
-    body: Option<string>;
+    body: Option;
     rawResponse: boolean
 };
 
 export type GetRequestConfig = {
     headers?: RequestHttpHeaders;
     query?: Record<string, string | number | boolean>;
+    rawResponse?: boolean
+};
+
+export type PostRequestConfig = {
+    headers?: RequestHttpHeaders;
+    query?: Record<string, string | number | boolean>;
+    body?: Option,
     rawResponse?: boolean
 };
 
@@ -51,22 +58,21 @@ export function buildHttpClient({ baseUrl = "", baseHeaders = {} }: HttpClientCo
             headers: headers.unsafeSource(),
         };
 
-        // if (body !== undefined && method !== HttpMethod.GET && method !== HttpMethod.DELETE) {
-        //     if (typeof body === "object" && !(body instanceof FormData)) {
-        //         init.headers = { "Content-Type": "application/json", ...init.headers };
-        //         const jsonString = json.toString(body);
-        //         if (jsonString.isErr()) {
-        //             return jsonString.mapErr(err => {
-        //                 return new HttpResponse({
-        //                     status: 1000,
-        //                     statusText: err.message,
-        //                 })
-        //             })
-        //         }
-        //     } else {
-        //         init.body = body;
-        //     }
-        // }
+        if (method !== HttpMethod.GET && method !== HttpMethod.DELETE) {
+            const bodyParsed = body.mapSome((body) => {
+                if (typeof body === "object" && !(body instanceof FormData)) {
+                    const jsonString = json.toString(body);
+                    if (jsonString.isErr()) {
+                        return jsonString.mapErr(err => {
+                            return new HttpResponse({
+                                status: 1000,
+                                statusText: err.message,
+                            })
+                        })
+                    }
+                }
+            })
+        }
 
         try {
             const response = await fetch(finalUrl, requestInit);
@@ -92,7 +98,7 @@ export function buildHttpClient({ baseUrl = "", baseHeaders = {} }: HttpClientCo
                     })
                     .flat()
 
-                if (! response.ok) {
+                if (!response.ok) {
                     return Err(new HttpResponse({
                         status: response.status,
                         statusText: response.statusText,
@@ -121,9 +127,11 @@ export function buildHttpClient({ baseUrl = "", baseHeaders = {} }: HttpClientCo
     }
 
     return {
-
-        get<T = unknown>(url, { query = {}, headers = new RequestHttpHeaders, rawResponse = false}: GetRequestConfig = {}) {
+        get<T = unknown>(url, { query = {}, headers = new RequestHttpHeaders, rawResponse = false }: GetRequestConfig = {}) {
             return request<T>(url, { query, headers, rawResponse, method: HttpMethod.GET, body: None() });
+        },
+        post<T = unknown>(url, { query = {}, headers = new RequestHttpHeaders, body = None(), rawResponse = false }: PostRequestConfig = {}) {
+            return request<T>(url, { query, headers, rawResponse, method: HttpMethod.POST, body });
         },
     }
 }
