3cff6c3c7d883a91a2b2d28ed099203f55db22af
Ratushniak DA <ratushniak_da@groupbwt.com>
2026-01-02 22:57:54 +0200

    std: fixed regexp asyncReplace

--- smuzi/std/src/regexp.ts
+++ smuzi/std/src/regexp.ts
@@ -4,25 +4,57 @@ import {promise} from "#lib/promise.js";
 import {dump} from "#lib/debug.js";
 
 export const regexp = {
+    // async asyncReplace<E extends StdError>(
+    //     str: string,
+    //     regex: RegExp,
+    //     asyncCallback: (...args) => Promise<Result<string, E>>): Promise<Result<string, StdError | E>> {
+    //     const promises = new Array<Promise<Result<string, E>>>;
+    //     const matches: unknown[] = [];
+    //
+    //     str.replace(regex, (...args) => {
+    //         promises.push(asyncCallback(...args));
+    //         matches.push(args);
+    //         return ''; // временное значение
+    //     });
+    //
+    //     return (await promise.all(promises)).mapOk(
+    //         (results) => {
+    //             return str.replace(regex, () => results.shift()?.okOr('') ?? '');
+    //
+    //     });
+    // },
+
     async asyncReplace<E extends StdError>(
         str: string,
-        regex: RegExp,
+        reg: RegExp,
         asyncCallback: (...args) => Promise<Result<string, E>>
     ): Promise<Result<string, E | StdError>> {
         //TODO: needs optimization, one loop instead of two call "replace"
-        const promises = new Array<Promise<Result<string, E>>>;
+        let resultStr = '';
+        let startSlice = 0;
+        let matched = false;
+        for (const match of str.matchAll(reg)) {
+            matched = true
+            const replaceResult = await asyncCallback(...match)
 
-        str.replace(regex, (...args) => {
-            promises.push(asyncCallback(...args));
-            return '';
-        });
+            if (replaceResult.isErr()) {
+                return replaceResult;
+            }
 
-        const res = (await promise.all(promises));
-        return res.mapOk(
-            (results) => {
-                let index = 0;
-                return  str.replace(regex, () => results[index++].okOr(''));
-        });
+            replaceResult.okThen(replacement => {
+                resultStr += str.slice(startSlice, match.index) + replacement;
+                startSlice = match.index + match[0].length;
+            })
+        }
 
+        if (! matched) {
+            return Ok(str)
+        }
+
+        if (startSlice < str.length) {
+            resultStr += str.slice(startSlice)
+        }
+
+        return Ok(resultStr)
     }
 }
