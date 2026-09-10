e4bcc328154298de03dd593f2f32a1fb7f9b074b
dinisimys2018 <dinisimys2018@gmail.com>
2026-09-10 18:02:26 +0300

    database: add prefix for migrations commands

--- smuzi/database/src/commands/router.ts
+++ smuzi/database/src/commands/router.ts
@@ -5,14 +5,14 @@ import migrationsRefresh from "./migrations/refresh.js";
 import migrationsFresh from "./migrations/fresh.js";
 
 
-export const databaseConsole = (config) => {
-    const router = CreateConsoleRouter('db:');
+export const databaseConsole = (db_service, prefix_command = "db:") => {
+    const router = CreateConsoleRouter(prefix_command);
     const routerMigrations = CreateConsoleRouter('migrations:');
 
-    routerMigrations.add('run', migrationsRun(config))
-    routerMigrations.add('rollback', migrationsRollback(config))
-    routerMigrations.add('refresh', migrationsRefresh(config))
-    routerMigrations.add('fresh', migrationsFresh(config))
+    routerMigrations.add('run', migrationsRun(db_service))
+    routerMigrations.add('rollback', migrationsRollback(db_service))
+    routerMigrations.add('refresh', migrationsRefresh(db_service))
+    routerMigrations.add('fresh', migrationsFresh(db_service))
 
     router.group(routerMigrations);
 
